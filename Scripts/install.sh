7ccd4f31d07bd77ef27b27aaf88fbeaccd20531e
Michael Dahl <Michael_Dahl@Michaels-MBP.attlocal.net>
2016-02-01 00:18:19 -0800

    Curl url is hopefully correct now

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,7 +6,7 @@
 # This link changes from time to time. I haven't found a reliable   installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from http://unity3d.com/get-unity/download?thank-you=personal&download_nid=26429&os=Mac: '
-curl -o Unity.pkg http://unity3d.com/get-unity/download?thank-you=personal&download_nid=26429&os=Mac
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/e87ab445ead0/MacEditorInstaller/Unity.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
