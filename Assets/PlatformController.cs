e2a4c23cb81d689474ede126a0b943a4a74614a2
Michael Dahl <michaeldahl7@gmail.com>
2016-02-10 20:57:59 -0800

    Moving platform work

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+    [SerializeField]
+    int moveDistance = 3;
+    [SerializeField]
+    float moveSpeed = 2;
+    Vector3 position;
+
+    // Use this for initialization
+    void Start () {
+        position = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (position != null) {
+            position.x += moveSpeed * Time.deltaTime;
+            transform.position = position;
+        }
+
+        if (position.x < -moveDistance)
+            moveSpeed = Mathf.Abs(moveSpeed);
+        else if (position.x > moveDistance)
+            moveSpeed = -Mathf.Abs(moveSpeed);
+	}
+}
